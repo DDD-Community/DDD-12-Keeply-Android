56f098e59e1414741ad7b03999c50165ad0f691f
cherry <cherryzp9094@gmail.com>
2025-07-15 00:06:33 +0900

    feat: TabBar 추가

---
+++ presentation/src/main/java/com/keeply/presentation/core/components/TabBar.kt
@@ -0,0 +1,123 @@
+package com.keeply.presentation.core.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.keeply.presentation.core.theme.KeeplyTheme
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+@Composable
+fun KeeplyTab(
+    selectedTabIndex: Int,
+    tabs: ImmutableList<String>,
+    onClick: (Int) -> Unit,
+) {
+    TabRow(
+        selectedTabIndex = selectedTabIndex,
+        containerColor = Color.Transparent,
+        indicator = { tabPositions ->
+            if (tabPositions.isNotEmpty()) {
+                val currentTabPosition = tabPositions[selectedTabIndex]
+                val indicatorStartPadding = if (selectedTabIndex == 0) 16.dp else 0.dp
+                val indicatorEndPadding = if (selectedTabIndex == tabPositions.lastIndex) 16.dp else 0.dp
+                Box(
+                    Modifier
+                        .tabIndicatorOffset(currentTabPosition)
+                        .fillMaxWidth()
+                        .height(2.dp)
+                        .padding(
+                            start = indicatorStartPadding,
+                            end = indicatorEndPadding
+                        )
+                        .background(
+                            color = KeeplyTheme.colors.neutralBlack
+                        )
+                )
+            }
+        },
+        divider = {
+            HorizontalDivider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(2.dp)
+            )
+        }
+    ) {
+        tabs.forEachIndexed { index, title ->
+            val isSelected = selectedTabIndex == index
+            val startPadding = if (index == 0) 16.dp else 0.dp
+            val endPadding = if (index == tabs.lastIndex) 16.dp else 0.dp
+
+            Tabs(
+                modifier = Modifier
+                    .padding(
+                        start = startPadding,
+                        end = endPadding
+                    ),
+                title = title,
+                index = index,
+                isSelected = isSelected,
+                onClick = onClick
+            )
+        }
+    }
+}
+
+@Composable
+private fun Tabs(
+    title: String,
+    index: Int,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: (Int) -> Unit
+) {
+    Tab(
+        selected = isSelected,
+        onClick = { onClick(index) },
+        modifier = modifier.height(48.dp),
+        content = {
+            val titleColor: Color = if (isSelected) {
+                KeeplyTheme.colors.neutralBlack
+            } else {
+                KeeplyTheme.colors.neutral400
+            }
+
+            KeeplyText(
+                text = title,
+                style = KeeplyTheme.typography.subtitle01,
+                color = titleColor,
+                maxLines = 1
+            )
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun KeeplyTabPreview() {
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val tabs = persistentListOf("Keeply", "안칠수")
+    KeeplyTab(
+        selectedTabIndex = 0,
+        tabs = tabs,
+        onClick = { index ->
+            selectedTabIndex = index
+        }
+    )
+}
