bea62e07e416ed8c6b16677399234e0f1a564e0c
cherry <cherryzp9094@gmail.com>
2025-07-15 00:06:48 +0900

    feat: TabBar 예제 HomeScreen 추가

--- presentation/src/main/java/com/keeply/presentation/ui/home/HomeContract.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/home/HomeContract.kt
@@ -1,10 +1,14 @@
 package com.keeply.presentation.ui.home
 
 import androidx.compose.runtime.Immutable
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Immutable
 data class HomeState(
-    val name: String = "Keeply"
+    val name: String = "Keeply",
+    val tabs: ImmutableList<String> = persistentListOf("Keeply", "안칠수"),
+    val selectedTabIndex: Int = 0
 )
 
 sealed interface HomeSideEffect {
