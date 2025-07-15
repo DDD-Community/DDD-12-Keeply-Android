6c822d4f3c6a523db3fe16ac120d81168d6e1bbb
cherry <cherryzp9094@gmail.com>
2025-07-15 23:09:06 +0900

    feat: HomeScreen에 InsightTextField 예제 생성

--- presentation/src/main/java/com/keeply/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/home/HomeScreen.kt
@@ -3,12 +3,14 @@ package com.keeply.presentation.ui.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.keeply.presentation.core.components.InsightTextField
 import com.keeply.presentation.core.components.KeeplyTab
 import com.keeply.presentation.core.components.KeeplyText
 import com.keeply.presentation.core.theme.KeeplyTheme
@@ -25,7 +27,10 @@ fun HomeRoute(
     HomeScreen(
         tabs = uiState.tabs,
         selectedTabIndex = uiState.selectedTabIndex,
-        onClickTab = viewModel::onClickTab
+        textField = uiState.textField,
+        textFieldMaxLength = uiState.textFieldMaxLength,
+        onClickTab = viewModel::onClickTab,
+        onValueChange = viewModel::onValueChange
     )
 }
 
@@ -33,11 +38,15 @@ fun HomeRoute(
 fun HomeScreen(
     tabs: ImmutableList<String>,
     selectedTabIndex: Int,
-    onClickTab: (Int) -> Unit
+    textField: String,
+    textFieldMaxLength: Int,
+    onClickTab: (Int) -> Unit,
+    onValueChange: (String) -> Unit,
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         KeeplyTab(
@@ -51,6 +60,13 @@ fun HomeScreen(
             style = KeeplyTheme.typography.header01,
             color = KeeplyTheme.colors.success
         )
+
+        InsightTextField(
+            value = textField,
+            placeholder = "인사이트를 적어주세요.",
+            onValueChange = onValueChange,
+            maxLength = textFieldMaxLength
+        )
     }
 }
 
@@ -60,6 +76,9 @@ fun HomeScreenPreview() {
     HomeScreen(
         tabs = persistentListOf("Keeply", "안칠수"),
         selectedTabIndex = 0,
-        onClickTab = {}
+        textField = "",
+        textFieldMaxLength = 300,
+        onClickTab = {},
+        onValueChange = {}
     )
 }
