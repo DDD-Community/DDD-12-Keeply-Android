68e5699e1871aa9ffb2259bcb4e3c34e993af16c
cherry <cherryzp9094@gmail.com>
2025-07-15 23:09:44 +0900

    style: presentation/core/ navigation 레이어 오타 수정

--- presentation/src/main/java/com/keeply/presentation/ui/home/navigation/HomeNavigator.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/home/navigation/HomeNavigator.kt
@@ -2,7 +2,7 @@ package com.keeply.presentation.ui.home.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.keeply.presentation.core.naviagion.HomeRoute
+import com.keeply.presentation.core.navigation.HomeRoute
 import com.keeply.presentation.ui.home.HomeRoute
 
 fun NavGraphBuilder.homeNavGraph() {
