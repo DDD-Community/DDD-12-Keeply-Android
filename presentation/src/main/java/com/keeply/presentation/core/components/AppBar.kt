a629c1f7f240d1dcfb6a6cb57db8bd5f8703cb69
cherry <cherryzp9094@gmail.com>
2025-07-19 16:12:32 +0900

    feat: appBar trailingText default 함수로 변경

--- presentation/src/main/java/com/keeply/presentation/core/components/AppBar.kt
+++ presentation/src/main/java/com/keeply/presentation/core/components/AppBar.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.keeply.domain.extend.default
 import com.keeply.presentation.core.theme.KeeplyTheme
 
 @Composable
@@ -70,7 +71,7 @@ fun KeeplyAppBar(
 
         if (trailingText.isNullOrEmpty().not()) {
             KeeplyText(
-                text = trailingText ?: "",
+                text = trailingText.default(),
                 style = KeeplyTheme.typography.subtitle01,
                 color = contentColor,
                 modifier = Modifier
