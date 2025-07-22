6b9a8eedcc6cba918cf6e8650adf867352ce3787
cherry <cherryzp9094@gmail.com>
2025-07-23 01:32:33 +0900

    feat: 앱 버전 표시를 위한 확장 함수 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         applicationId = "com.keeply.kr"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         
         buildConfigField(
             "String",
