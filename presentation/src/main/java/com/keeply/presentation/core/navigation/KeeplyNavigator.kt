811edc2a3518e201e5625f2482f02b35144994d4
SeongKwang Lee <75346193+cherryzp9094@users.noreply.github.com>
2025-07-24 22:51:19 +0900

    Revert "[GBIZ-102] splash 화면 추가 (#17)"
    This reverts commit 4c621a4614ed394cc349f1d2643f70282a055df7.

--- presentation/src/main/java/com/keeply/presentation/core/navigation/KeeplyNavigator.kt
+++ presentation/src/main/java/com/keeply/presentation/core/navigation/KeeplyNavigator.kt
@@ -1,27 +1,24 @@
 package com.keeply.presentation.core.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
 import com.keeply.presentation.ui.alarm.navigation.navigateAlarm
 import com.keeply.presentation.ui.folder.navigation.navigateFolder
 import com.keeply.presentation.ui.home.navigation.navigateHome
 import com.keeply.presentation.ui.my.navigation.navigateMy
-import com.keeply.presentation.ui.onboarding.navigation.navigateOnboarding
 import com.keeply.presentation.ui.scan.navigation.navigateScan
 
 @Stable
 class KeeplyNavigator(
     val navController: NavHostController
 ) {
-    val startDestination = OnboardingRoute.Onboarding
+    val startDestination = HomeRoute.Home
 
     fun popBackStack() {
         navController.popBackStack()
@@ -44,13 +41,6 @@ class KeeplyNavigator(
             KeeplyTab.MY -> navController.navigateMy(navOptions)
         }
     }
-
-    fun navigateOnboarding() = navController.navigateOnboarding()
-
-    @Composable
-    fun shouldShowNavigationBar() = KeeplyTab.contains {
-        navController.currentDestination?.hasRoute(it::class) == true
-    }
 }
 
 enum class KeeplyTab(
@@ -70,14 +60,7 @@ enum class KeeplyTab(
     ),
     MY(
         route = HomeRoute.My
-    );
-
-    companion object {
-        @Composable
-        fun contains(predicate: @Composable (HomeRoute) -> Boolean): Boolean {
-            return KeeplyTab.entries.map { it.route }.any { predicate(it) }
-        }
-    }
+    )
 }
 
 @Composable
--- presentation/src/main/java/com/keeply/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/main/MainActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.SystemBarStyle
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,50 +22,19 @@ import com.keeply.presentation.core.navigation.KeeplyNavHost
 import com.keeply.presentation.core.navigation.KeeplyTab
 import com.keeply.presentation.core.navigation.rememberKeeplyNavigator
 import com.keeply.presentation.core.theme.KeeplyTheme
-import com.keeply.presentation.ui.splash.SplashScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.light(
-                android.graphics.Color.TRANSPARENT,
-                android.graphics.Color.TRANSPARENT
-            ),
-            navigationBarStyle = SystemBarStyle.light(
-                android.graphics.Color.TRANSPARENT,
-                android.graphics.Color.TRANSPARENT
-            )
-        )
+        enableEdgeToEdge()
 
         setContent {
             KeeplyTheme {
-                val backgroundColor = KeeplyTheme.colors.neutral100
                 val keeplyNavigator = rememberKeeplyNavigator()
                 var selectedTab by remember { mutableStateOf(KeeplyTab.HOME) }
-                var isShowSplash by remember { mutableStateOf(true) }
-
-                LaunchedEffect(Unit) {
-                    delay(2000)
-                    isShowSplash = false
-                }
-
-                SideEffect {
-                    enableEdgeToEdge(
-                        statusBarStyle = SystemBarStyle.light(
-                            backgroundColor.toArgb(),
-                            backgroundColor.toArgb()
-                        ),
-                        navigationBarStyle = SystemBarStyle.light(
-                            backgroundColor.toArgb(),
-                            backgroundColor.toArgb()
-                        )
-                    )
-                }
-
+                
                 Scaffold(
                     modifier = Modifier
                         .fillMaxSize()
@@ -83,27 +48,21 @@ class MainActivity : ComponentActivity() {
                     ) {
                         KeeplyNavHost(keeplyNavigator)
 
-                        if (keeplyNavigator.shouldShowNavigationBar()) {
-                            NavigationBar(
+                        NavigationBar(
                                 modifier = Modifier
                                     .padding(bottom = 16.dp)
                                     .align(Alignment.BottomCenter),
-                                selectedTab = selectedTab,
-                                onTabSelected = { tab ->
-                                    selectedTab = tab
+                            selectedTab = selectedTab,
+                            onTabSelected = { tab ->
+                                selectedTab = tab
 
-                                    keeplyNavigator.navigate(
-                                        KeeplyTab.entries.find {
-                                            it == selectedTab
-                                        } ?: KeeplyTab.HOME
-                                    )
-                                }
-                            )
-                        }
-
-                        if(isShowSplash) {
-                            SplashScreen()
-                        }
+                                keeplyNavigator.navigate(
+                                    KeeplyTab.entries.find {
+                                        it == selectedTab
+                                    } ?: KeeplyTab.HOME
+                                )
+                            }
+                        )
                     }
                 }
             }
