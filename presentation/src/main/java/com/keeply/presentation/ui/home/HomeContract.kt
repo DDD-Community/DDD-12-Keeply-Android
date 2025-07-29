99e98fa665b8988621ac6423e55a5758246b11b9
cherry <cherryzp9094@gmail.com>
2025-07-29 23:05:13 +0900

    feat: Home KeeplyAppBar 추가

--- presentation/src/main/java/com/keeply/presentation/ui/home/HomeContract.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/home/HomeContract.kt
@@ -1,19 +1,10 @@
 package com.keeply.presentation.ui.home
 
 import androidx.compose.runtime.Immutable
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.persistentListOf
 
 @Immutable
 data class HomeState(
-    val name: String = "Keeply",
-    val tabs: ImmutableList<String> = persistentListOf("Keeply", "안칠수"),
-    val selectedTabIndex: Int = 0,
-    val textField: String = "",
-    val textFieldMaxLength: Int = 300,
-    val isTagChecked: Boolean = false
+    val isLoading: Boolean = false,
 )
 
-sealed interface HomeSideEffect {
-
-}
+sealed interface HomeSideEffect
