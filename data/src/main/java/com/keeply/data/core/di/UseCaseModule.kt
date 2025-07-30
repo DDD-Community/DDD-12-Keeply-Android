050b2a232183ad4c2a6de87f08346847b2a42147
cherry <cherryzp9094@gmail.com>
2025-07-30 22:52:11 +0900

    feat: GetLocalScreenshotsUseCase di 추가

--- data/src/main/java/com/keeply/data/core/di/UseCaseModule.kt
+++ data/src/main/java/com/keeply/data/core/di/UseCaseModule.kt
@@ -1,11 +1,14 @@
 package com.keeply.data.core.di
 
+import com.keeply.domain.repository.LocalScreenshotRepository
 import com.keeply.domain.repository.PreferencesRepository
 import com.keeply.domain.repository.UserRepository
 import com.keeply.domain.usecase.scan.GetScanOnBoardingVisibilityUseCase
 import com.keeply.domain.usecase.scan.GetScanOnBoardingVisibilityUseCaseImpl
 import com.keeply.domain.usecase.scan.SetDoNotShowDialogUseCase
 import com.keeply.domain.usecase.scan.SetDoNotShowDialogUseCaseImpl
+import com.keeply.domain.usecase.screenshot.GetLocalScreenshotsUseCase
+import com.keeply.domain.usecase.screenshot.GetLocalScreenshotsUseCaseImpl
 import com.keeply.domain.usecase.user.LoginKakaoUseCase
 import com.keeply.domain.usecase.user.LoginKakaoUseCaseImpl
 import dagger.Module
@@ -40,6 +43,14 @@ object UseCaseModule {
         preferencesRepository: PreferencesRepository
     ): SetDoNotShowDialogUseCase = SetDoNotShowDialogUseCaseImpl(
         repository = preferencesRepository
+    )
+
+    @Provides
+    @Singleton
+    fun provideGetLocalScreenshotsUseCase(
+        localScreenshotRepository: LocalScreenshotRepository
+    ): GetLocalScreenshotsUseCase = GetLocalScreenshotsUseCaseImpl(
+        repository = localScreenshotRepository
     )
 
 }
