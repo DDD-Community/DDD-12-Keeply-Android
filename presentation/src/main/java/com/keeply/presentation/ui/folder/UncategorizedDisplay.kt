7232dda55e24dc984a31d60dbeee305669c8f98d
cherry <cherryzp9094@gmail.com>
2025-07-31 23:49:01 +0900

    feat: 리뷰 변경사항 반영

--- presentation/src/main/java/com/keeply/presentation/ui/folder/UncategorizedDisplay.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/folder/UncategorizedDisplay.kt
@@ -1,8 +1,6 @@
 package com.keeply.presentation.ui.folder
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,9 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Label
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,20 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.keeply.presentation.R
-import com.keeply.presentation.core.components.ImageFrame
 import com.keeply.presentation.core.components.KeeplyIconButton
 import com.keeply.presentation.core.components.KeeplyText
 import com.keeply.presentation.core.components.Tag
 import com.keeply.presentation.core.theme.KeeplyTheme
-import com.keeply.presentation.ui.folder.component.FolderCardItem
 import com.keeply.presentation.ui.folder.component.UncategorizedCardItem
-import kotlin.collections.forEach
 
 @Composable
 fun UncategorizedDisplay() {
@@ -65,30 +53,6 @@ fun UncategorizedDisplay() {
             )
         }
 
-        Row(
-            modifier = Modifier
-                .padding(horizontal = 12.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            var labelCheck by remember { mutableStateOf(true) }
-
-            Tag(
-                label = "전체보기",
-                checked = labelCheck,
-                onCheckedChange = {
-                    labelCheck = true
-                }
-            )
-
-            Tag(
-                label = "오늘만료",
-                checked = labelCheck.not(),
-                onCheckedChange = {
-                    labelCheck = false
-                }
-            )
-        }
-
         val folderList = List(100) { it } // 예시 리스트
         val chunkedList = folderList.chunked(3)
 
@@ -96,18 +60,47 @@ fun UncategorizedDisplay() {
             modifier = Modifier
                 .fillMaxWidth(),
             contentPadding = PaddingValues(
-                top = 10.dp,
-                start = 16.dp,
-                end = 16.dp,
+                start = 12.dp,
+                end = 12.dp,
                 bottom = 112.dp
             )
         ) {
+            item {
+                Row(
+                    modifier = Modifier
+                        .padding(bottom = 10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    var labelCheck by remember { mutableStateOf(true) }
+
+                    Tag(
+                        label = "전체보기",
+                        checked = labelCheck,
+                        onCheckedChange = {
+                            labelCheck = true
+                        }
+                    )
+
+                    Tag(
+                        label = "오늘만료",
+                        checked = labelCheck.not(),
+                        onCheckedChange = {
+                            labelCheck = false
+                        }
+                    )
+                }
+            }
+
             items(chunkedList.size) { index ->
                 val rowItems = chunkedList[index]
 
                 Row(
                     modifier = Modifier
-                        .padding(bottom = 10.dp)
+                        .padding(
+                            start = 4.dp,
+                            end = 4.dp,
+                            bottom = 10.dp
+                        )
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
