723533ee3c3ae5a89d274b9c4598559ca3bb4834
cherry <cherryzp9094@gmail.com>
2025-08-02 01:02:18 +0900

    feat: 폴더 도메인 모델 및 유스케이스 추가

---
+++ domain/src/main/java/com/keeply/domain/folder/model/Folder.kt
@@ -0,0 +1,7 @@
+package com.keeply.domain.folder.model
+
+data class Folder(
+    val folderId: Int,
+    val folderName: String,
+    val color: String
+)
---
+++ domain/src/main/java/com/keeply/domain/folder/usecase/CreateFolderUseCase.kt
@@ -0,0 +1,14 @@
+package com.keeply.domain.folder.usecase
+
+import com.keeply.domain.folder.model.Folder
+import com.keeply.domain.folder.repository.FolderRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CreateFolderUseCase @Inject constructor(
+    private val folderRepository: FolderRepository
+) {
+    suspend operator fun invoke(folderName: String, color: String): Flow<Folder> {
+        return folderRepository.createFolder(folderName, color)
+    }
+}
