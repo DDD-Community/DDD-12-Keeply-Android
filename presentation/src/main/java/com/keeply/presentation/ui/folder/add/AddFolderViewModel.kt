1bd92b507872a7e1aa8fa89152b3d35cdc47cbd1
cherry <cherryzp9094@gmail.com>
2025-08-02 01:03:04 +0900

    feat: 폴더 생성 API 연동 구현

--- presentation/src/main/java/com/keeply/presentation/ui/folder/add/AddFolderViewModel.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/folder/add/AddFolderViewModel.kt
@@ -1,15 +1,22 @@
 package com.keeply.presentation.ui.folder.add
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.keeply.domain.extend.default
+import com.keeply.domain.folder.usecase.CreateFolderUseCase
 import com.keeply.presentation.core.components.colorBar.FolderColor
+import com.keeply.presentation.core.components.colorBar.toHexString
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
-class AddFolderViewModel @Inject constructor(): ViewModel(), ContainerHost<AddFolderState, AddFolderSideEffect> {
+class AddFolderViewModel @Inject constructor(
+    private val createFolderUseCase: CreateFolderUseCase
+): ViewModel(), ContainerHost<AddFolderState, AddFolderSideEffect> {
     override val container: Container<AddFolderState, AddFolderSideEffect> =
         container(AddFolderState())
     
@@ -22,7 +29,16 @@ class AddFolderViewModel @Inject constructor(): ViewModel(), ContainerHost<AddFo
     }
     
     fun createFolder() = intent {
-        postSideEffect(AddFolderSideEffect.ShowCreateSuccess)
-        postSideEffect(AddFolderSideEffect.NavigateBack)
+        val folderName = state.folderName
+        val color = state.folderColor.toHexString()
+        
+        createFolderUseCase(folderName, color)
+            .catch { exception ->
+                Log.e("test", exception.message.default())
+                // TODO: 에러 처리 - 에러 메시지 표시 등
+            }
+            .collect { folder ->
+                postSideEffect(AddFolderSideEffect.ShowCreateSuccess)
+            }
     }
 }
