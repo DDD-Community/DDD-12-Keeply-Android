8ad0c080c79414a19fe56e5712a19301342e9928
cherry <cherryzp9094@gmail.com>
2025-08-05 23:33:16 +0900

    feat: FolderModifyBottomSheetContent 컴포넌트 추가

--- presentation/src/main/java/com/keeply/presentation/ui/folder/detail/component/FolderModifyBottomSheetContent.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/folder/detail/component/FolderModifyBottomSheetContent.kt
@@ -1,2 +1,75 @@
 package com.keeply.presentation.ui.folder.detail.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.keeply.presentation.core.components.KeeplyButton
+import com.keeply.presentation.core.components.KeeplyButtonStyle
+import com.keeply.presentation.core.components.KeeplyTextField
+import com.keeply.presentation.core.components.colorBar.ColorBar
+import com.keeply.presentation.core.components.colorBar.FolderColor
+import com.keeply.presentation.core.theme.KeeplyTheme
+
+@Composable
+fun FolderModifyBottomSheetContent() {
+    Column(
+        modifier = Modifier
+            .padding(
+                top = 24.dp,
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 20.dp
+            )
+    ) {
+        KeeplyTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = "DDD_안칠수",
+            onValueChange = {}
+        )
+
+        ColorBar(
+            modifier = Modifier
+                .padding(top = 20.dp),
+            selectedColor = FolderColor.ORANGE
+        ) {
+
+        }
+
+        Row(
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            KeeplyButton(
+                modifier = Modifier
+                    .weight(1f),
+                text = "폴더 삭제",
+                onClick = {},
+                buttonStyle = KeeplyButtonStyle.SECONDARY
+            )
+
+            KeeplyButton(
+                modifier = Modifier
+                    .weight(1f),
+                text = "폴더 삭제",
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun FolderModifyBottomSheetContentPreview() {
+    KeeplyTheme {
+        FolderModifyBottomSheetContent()
+    }
+}
