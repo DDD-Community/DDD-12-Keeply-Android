20beab4abae64bdb38577c557425325a4c001bfc
cherry <cherryzp9094@gmail.com>
2025-08-05 23:01:21 +0900

    refactor: HomeKeeplyScreenshotItem 컴포넌트 파라미터 추가

--- presentation/src/main/java/com/keeply/presentation/ui/home/component/HomeKeeplyScreenshotItem.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/home/component/HomeKeeplyScreenshotItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.keeply.presentation.core.components.FileTag
@@ -19,7 +20,11 @@ import com.keeply.presentation.core.theme.KeeplyTheme
 
 @Composable
 fun HomeKeeplyScreenshotItem(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    painter: Painter? = null,
+    tag: String = "",
+    date: String = "",
+    insight: String = ""
 ) {
     Row(
         modifier = modifier
@@ -27,7 +32,7 @@ fun HomeKeeplyScreenshotItem(
         ImageFrame(
             modifier = Modifier
                 .width(70.dp),
-            painter = ColorPainter(KeeplyTheme.colors.neutral200)
+            painter = painter ?: ColorPainter(KeeplyTheme.colors.neutral200)
         )
 
         Column(
@@ -42,19 +47,19 @@ fun HomeKeeplyScreenshotItem(
                 FileTag(
                     modifier = Modifier
                         .weight(1f),
-                    text = "독서 인사이트",
+                    text = tag,
                     style = FileTagStyle.Small
                 )
 
                 KeeplyText(
-                    text = "2025.06.18",
+                    text = date,
                     style = KeeplyTheme.typography.subtitle03,
                     color = KeeplyTheme.colors.neutral600
                 )
             }
 
             KeeplyText(
-                text = "역시 '공감'은 UX의 시작점이자 끝이다. 내가 사용자 입장에서 느낀 불편을 너무 디자이너 시선으로만 풀고 있다...",
+                text = insight,
                 style = KeeplyTheme.typography.body,
                 color = KeeplyTheme.colors.neutral900,
                 maxLines = 3
