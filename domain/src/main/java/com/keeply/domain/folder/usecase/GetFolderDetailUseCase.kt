b073fd493759db40fe8d1d80229d9851071f0192
cherry <cherryzp9094@gmail.com>
2025-08-05 22:57:16 +0900

    feat: GetFolderDetailUseCase 구현

---
+++ domain/src/main/java/com/keeply/domain/folder/usecase/GetFolderDetailUseCase.kt
@@ -0,0 +1,14 @@
+package com.keeply.domain.folder.usecase
+
+import com.keeply.domain.folder.model.FolderImage
+import com.keeply.domain.folder.repository.FolderRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetFolderDetailUseCase @Inject constructor(
+    private val folderRepository: FolderRepository
+) {
+    suspend operator fun invoke(folderId: Long): Flow<List<FolderImage>> {
+        return folderRepository.getFolderDetail(folderId)
+    }
+}
