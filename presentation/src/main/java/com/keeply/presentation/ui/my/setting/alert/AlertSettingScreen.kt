b747fd9241a5c4dad338b42789285a8f4f4c910a
cherry <cherryzp9094@gmail.com>
2025-08-16 14:32:47 +0900

    ui: 알림 설정 화면 UI 개선

--- presentation/src/main/java/com/keeply/presentation/ui/my/setting/alert/AlertSettingScreen.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/my/setting/alert/AlertSettingScreen.kt
@@ -3,17 +3,21 @@ package com.keeply.presentation.ui.my.setting.alert
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.keeply.presentation.core.components.KeeplyAppBar
+import com.keeply.presentation.core.components.KeeplyToggle
 import com.keeply.presentation.core.theme.KeeplyTheme
+import com.keeply.presentation.ui.my.setting.component.SettingToggleItem
 
 @Composable
 fun SettingAlertRoute(
-    viewModel: SettingAlertViewModel = hiltViewModel()
+    viewModel: SettingAlertViewModel = hiltViewModel(),
 ) {
     SettingAlertScreen()
 }
@@ -34,8 +38,35 @@ fun SettingAlertScreen() {
                     tint = KeeplyTheme.colors.neutralBlack
                 )
             },
-            onClickLeading = {  },
+            onClickLeading = { },
         )
+
+        Column(
+            modifier = Modifier
+                .padding(
+                    top = 11.dp,
+                    start = 16.dp,
+                    end = 16.dp
+                )
+        ) {
+            SettingToggleItem(
+                text = "저장 용량 알림",
+                isChecked = true,
+                onToggleValueChange = {}
+            )
+
+            SettingToggleItem(
+                text = "마케팅 알림",
+                isChecked = false,
+                onToggleValueChange = {}
+            )
+
+            SettingToggleItem(
+                text = "알림 권한",
+                isChecked = false,
+                onToggleValueChange = {}
+            )
+        }
     }
 }
 
--- presentation/src/main/java/com/keeply/presentation/ui/my/setting/component/SettingToggleItem.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/my/setting/component/SettingToggleItem.kt
@@ -1,2 +1,52 @@
 package com.keeply.presentation.ui.my.setting.component
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.keeply.presentation.core.components.KeeplyText
+import com.keeply.presentation.core.components.KeeplyToggle
+import com.keeply.presentation.core.theme.KeeplyTheme
+
+@Composable
+fun SettingToggleItem(
+    modifier: Modifier = Modifier,
+    text: String,
+    isChecked: Boolean = false,
+    onToggleValueChange: (Boolean) -> Unit = {},
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(46.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        KeeplyText(
+            modifier = Modifier
+                .weight(1f),
+            text = text,
+            style = KeeplyTheme.typography.caption01,
+        )
+
+        KeeplyToggle(
+            checked = isChecked,
+            onClick = onToggleValueChange
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingToggleItemPreview() {
+    KeeplyTheme {
+        SettingToggleItem(
+            text = "알림 설정",
+            isChecked = true,
+        )
+    }
+}
