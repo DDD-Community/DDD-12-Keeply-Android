b0ceb86d049eb7d28d550122aa248fbcc8143d37
cherry <cherryzp9094@gmail.com>
2025-08-24 23:11:31 +0900

    feat: CropViewModel에 URI 파라미터 처리 로직 구현

--- presentation/src/main/java/com/keeply/presentation/ui/scan/crop/CropViewModel.kt
+++ presentation/src/main/java/com/keeply/presentation/ui/scan/crop/CropViewModel.kt
@@ -1,6 +1,9 @@
 package com.keeply.presentation.ui.scan.crop
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.toRoute
+import com.keeply.presentation.ui.scan.navigation.ScanRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.Container
 import org.orbitmvi.orbit.ContainerHost
@@ -8,8 +11,14 @@ import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
-class CropViewModel @Inject constructor(): ContainerHost<CropState, CropSideEffect>,ViewModel() {
-    override val container: Container<CropState, CropSideEffect> = container(CropState())
-
+class CropViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+): ContainerHost<CropState, CropSideEffect>,ViewModel() {
+    
+    private val scanCrop = savedStateHandle.toRoute<ScanRoute.ScanCrop>()
+    
+    override val container: Container<CropState, CropSideEffect> = container(
+        CropState(uri = scanCrop.url)
+    )
 
 }
