62f930b752818d9e93b44fffc7bff02edb713e50
cherry <cherryzp9094@gmail.com>
2025-08-26 23:52:49 +0900

    feat: 이미지 저장 API 인터페이스 추가

--- data/src/main/java/com/keeply/data/image/remote/ImageService.kt
+++ data/src/main/java/com/keeply/data/image/remote/ImageService.kt
@@ -3,12 +3,22 @@ package com.keeply.data.image.remote
 import com.keeply.data.core.base.BaseResponse
 import com.keeply.data.dto.image.CreateImageRequest
 import com.keeply.data.dto.image.ImageResponse
+import com.keeply.data.screenshot.model.ImageSaveResponse
+import okhttp3.MultipartBody
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ImageService {
     @POST("api/images")
     suspend fun createImage(
         @Body request: CreateImageRequest
     ): BaseResponse<ImageResponse>
+    
+    @Multipart
+    @POST("api/images/save")
+    suspend fun saveImage(
+        @Part file: MultipartBody.Part
+    ): BaseResponse<ImageSaveResponse>
 }
