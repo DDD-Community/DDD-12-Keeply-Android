b00110b0d1f07df4fd6c8d115997ff5d7dbf4f2e
yujinlee <leeyuding@gmail.com>
2025-08-26 20:55:21 +0900

    NavigationBar 스샷추가 아이콘 수정

--- presentation/src/main/java/com/keeply/presentation/core/components/NavigationBar.kt
+++ presentation/src/main/java/com/keeply/presentation/core/components/NavigationBar.kt
@@ -2,12 +2,10 @@ package com.keeply.presentation.core.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,7 +14,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -90,9 +87,9 @@ private fun NavigationBarCenterItem(
         contentAlignment = Alignment.Center
     ) {
         Icon(
-            painter = KeeplyTheme.icons.scanAlt,
+            painter = KeeplyTheme.icons.add,
             contentDescription = "Scan",
-            modifier = Modifier.size(24.dp),
+            modifier = Modifier.size(28.dp),
             tint = Color.White
         )
     }
